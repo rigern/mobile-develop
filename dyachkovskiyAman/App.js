b5f8ec24b3d71e27b2c5f986749039b4a8151e69
rggrgr <aman.dyachkovskiy.00@mail.ru>
2025-01-15 02:58:44 +0900

    change to expo

---
+++ dyachkovskiyAman/App.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+import Lab1 from "./components/lab1";
+import React from "react";
+
+const App = () => {
+  return <Lab1 />;
+};
+
+export default App;
